3465b12a7eea4a57b9b807e9df7787af1fdfd3fa
Mohammed Shuaib Shaik <65175177+shuaibee@users.noreply.github.com>
2022-05-15 18:23:03 +0400

    Add files via upload

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,39 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include,path
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("""Hello, Ms. Hailey. You're at the Homepage. This is a demonstration of what i've learnt from the tutorials. :))
+                           To view the index page, add /firstapp to the url""")
+
+# View to change boring default django home page.
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('firstapp/', include('firstapp.urls')),
+    path('', index),
+]
+
+
+
+
+
+
+
+
